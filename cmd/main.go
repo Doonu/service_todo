17046df8baea96cfc5f45933e88405dcbe2509b2
Doonu <DonuSuzerain@yandex.ru>
2024-09-08 21:09:28 +0300

    feat: добавлена базовая регистрация

--- cmd/main.go
+++ cmd/main.go
@@ -3,22 +3,24 @@ package main
 import (
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
-	"log"
 	"os"
 	todo "service_todo"
-	"service_todo/pkg/hanlders"
+	"service_todo/pkg/handlers"
 	"service_todo/pkg/repository"
 	"service_todo/pkg/service"
 )
 
 func main() {
+	logrus.SetFormatter(new(logrus.JSONFormatter))
+
 	if err := initConfig(); err != nil {
-		log.Fatalf("error ininitializing config: %s", err.Error())
+		logrus.Fatalf("error ininitializing config: %s", err.Error())
 	}
 
 	if err := godotenv.Load(); err != nil {
-		log.Fatalf("error loading env variables")
+		logrus.Fatalf("error loading env variables")
 	}
 
 	db, err := repository.NewPostgresDB(repository.Config{
@@ -31,17 +33,22 @@ func main() {
 	})
 
 	if err != nil {
-		log.Fatalf("failed to initialize db: %s", err.Error())
+		logrus.Fatalf("failed to initialize db: %s", err.Error())
 	}
 
 	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
-	handlers := hanlders.NewHandler(services)
+	hand := handlers.NewHandler(services)
 
 	srv := new(todo.Server)
-	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
-		log.Fatalf("error occured while running http server: %s", err.Error())
+
+	router := hand.InitRoutes()
+
+	if err := srv.Run(viper.GetString("port"), router); err != nil {
+		logrus.Fatalf("error occured while running http server: %s", err.Error())
 	}
+
+	logrus.Println("Server running on port 8080")
 }
 
 func initConfig() error {
--- pkg/hanlders/auth.go
+++
@@ -1,9 +0,0 @@
-package hanlders
-
-import "github.com/gin-gonic/gin"
-
-func (h *Handler) signUp(c *gin.Context) {
-}
-
-func (h *Handler) signIn(c *gin.Context) {
-}
